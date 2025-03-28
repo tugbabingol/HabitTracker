125783c4c1c49d5f9fa790a3e7548dda066633ea
tugbabingol <tugbaabiingoll@gmail.com>
2025-03-28 21:28:08 +0300

    collectAsState hatası düzeltildi

--- app/src/main/java/com/tugbabingol/habittracker/ui/theme/Screen/AddHabitScreen.kt
+++ app/src/main/java/com/tugbabingol/habittracker/ui/theme/Screen/AddHabitScreen.kt
@@ -3,6 +3,10 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ShaderBrush
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.tugbabingol.habittracker.Model.HabitEntity
@@ -13,16 +17,33 @@ fun AddHabitScreen(navController: NavController, viewModel: HabitViewModel, modi
     var title by remember { mutableStateOf("") }
     var description by remember { mutableStateOf("") }
 
+//    val rainbowColors = listOf(
+//        Color.Red,
+//        Color.Yellow,
+//        Color.Green,
+//        Color.Blue,
+//        Color.Magenta,
+//        Color.White
+//    )
+//
+//    val brush = remember {
+//        Brush.linearGradient(
+//            colors = rainbowColors
+//        )
+//    }
+
+
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(top = 16.dp, start = 10.dp, end = 10.dp )
+            .padding(top = 16.dp, start = 10.dp, end = 10.dp)
     ) {
         OutlinedTextField(
             value = title,
             onValueChange = { title = it },
             label = { Text("Title") },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+//            textStyle = TextStyle(brush = brush)
         )
 
         Spacer(modifier = Modifier.height(16.dp))
--- app/src/main/java/com/tugbabingol/habittracker/ui/theme/Screen/HomeScreen.kt
+++ app/src/main/java/com/tugbabingol/habittracker/ui/theme/Screen/HomeScreen.kt
@@ -1,6 +1,4 @@
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,40 +8,36 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
-import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.tugbabingol.habittracker.ViewModel.HabitViewModel
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBars
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.draw.clip
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+
+
+
 
 @Composable
 fun HomeScreen(navController: NavController, viewModel: HabitViewModel, modifier: Modifier) {
-    val habits by viewModel.allHabits.collectAsState()
+    val habits by viewModel.allHabits.collectAsStateWithLifecycle(initialValue = emptyList())
 
-    val bottomPadding = with(LocalDensity.current) {
-        WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
-    }
+
+
+
+//    val bottomPadding = with(LocalDensity.current) {
+//        WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
+//    }
 
 
         Column(
